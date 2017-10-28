59b819af98ab950ef90e83599ba2b3eaea985e35
Jan Rydzewski <flegmer@gmail.com>
2017-10-28 15:36:47 +0200

    /tmp on local fs

--- ipfs_execute.sh
+++ ipfs_execute.sh
@@ -3,7 +3,7 @@ set -e
 
 function cleanup {
 	# cleanup
-	rm -rf rootfs output
+	rm -rf rootfs tmp output
 }
 trap cleanup EXIT
 
@@ -16,7 +16,8 @@ shift 2
 mkdir rootfs
 ipfs cat -- "$rootfs" | tar -xz -C rootfs --warning=no-timestamp
 
-# make output dir
+# make output and tmp dir
+mkdir tmp
 mkdir output
 
 # run task in bubblewrap
@@ -24,10 +25,11 @@ env -i `which bwrap` \
 	--unshare-all \
 	--uid 0 \
 	--gid 0 \
+	--die-with-parent \
 	--bind rootfs / \
 	--ro-bind "`ipfs resolve -- $input`" /input \
 	--bind output /output \
-	--tmpfs /tmp \
+	--bind tmp /tmp \
 	--proc /proc \
 	--dev /dev \
 	--remount-ro / \
