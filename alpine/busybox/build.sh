267550de3e9999f234c713c1671aed0dab266f5d
Jan Rydzewski <flegmer@gmail.com>
2017-10-28 15:15:19 +0200

    busybox build independent of time

--- alpine/busybox/build.sh
+++ alpine/busybox/build.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
-set -e
+set -e -x
+
+# make tempdir
+function cleanup {
+	# cleanup
+	rm -rf $D
+}
+trap cleanup EXIT
 D=$(mktemp -d)
+
+# obtain latest version of busybox-static
 V=$(ipfs ls -- $1/$2/ | grep busybox-static | tail -n1 | cut -d " " -f1)
+
+# extract and add custom init file
 ipfs cat -- $V | tar -xz -C $D
 mkdir $D/sbin
 cp init $D/sbin/init
-P=$(tar -c --mtime 1970-01-01 -C $D . | gzip -cn | ipfs add -Q --pin=false)
-rm -rf $D
+
+# remove timestamps - to ensure build result independent of time
+touch -d 1970-01-01 $D/sbin/init
+touch -d 1970-01-01 $D/sbin
+touch -d 1970-01-01 $D
+
+# copress and add to ipfs
+# gzip should not add time and name metadata: thus flag -n
+P=$(tar -c -C $D . | gzip -cn | ipfs add -Q --pin=false)
+
+# run building container
 ipfs-execute $P $(ipfs-mkdir rootfs.tar.gz $P) | \
 	awk '{print $1"/rootfs.tar.gz"}'
